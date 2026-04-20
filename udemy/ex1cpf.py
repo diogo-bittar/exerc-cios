2b03e10ef8fc9995645fc5517fdeae8a8c7df821
Diogo Bittar Pedro <bittard58@gmail.com>
2026-04-20 15:58:21 -0300

    Exercício resolvido

--- udemy/ex1cpf.py
+++ udemy/ex1cpf.py
@@ -33,15 +33,33 @@ O 1o dígito é 7.
 
 #CÁLCULO DO 1o DÍGITO
 
-cpf = '74682489070'
-nove_digitos = cpf[:9]
+cpf_enviado = '74682489070'
+nove_digitos = cpf_enviado[:9]
 cont_regressivo_1 = 10
-resultado = 0
+resultado1 = 0
 
-for digito_um in nove_digitos:
-    resultado += int(digito_um) * cont_regressivo_1
-    cont_regressivo -= 1
+for digito in nove_digitos:
+    resultado1 += int(digito) * cont_regressivo_1
+    cont_regressivo_1 -= 1
 
-digito_um = (resultado * 10) % 11
+digito_um = (resultado1 * 10) % 11
 digito_um = digito_um if digito_um <= 9 else 0
 #----------------------------------------------------
+dez_digitos = nove_digitos + str(digito_um)
+resultado2 = 0
+cont_regressivo2 = 11
+for digito in dez_digitos:
+    resultado2 += int(digito) * cont_regressivo2
+    cont_regressivo2 -= 1
+digito_dois = resultado2 * 10 % 11
+digito_dois = digito_dois if digito_dois <= 9 else 0 
+
+cpf_gerado = f'{nove_digitos}{digito_um}{digito_dois}'
+
+if cpf_enviado == cpf_gerado:
+    print(f'O CPF {cpf_enviado} é válido.')
+else:
+    print('O CPF é inválido.')
+
+#Dar uma revisada depois nesse exercício e tentar recriar do 0
+
