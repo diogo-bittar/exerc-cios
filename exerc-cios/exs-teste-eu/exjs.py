739fd735bfe510f973ad232bb026c5e5eed091a1
Diogo Bittar Pedro <bittard58@gmail.com>
2026-05-07 14:22:54 -0300

    ogg

--- exerc-cios/exs-teste-eu/exjs.py
+++
@@ -1,8 +0,0 @@
-while True:
-    nome = input('Informe o seu nome por favor: ')
-    if len(nome) < 3:
-        print('O nome precisa ter mais de 2 caracteres.')
-    elif not nome.replace(' ', '').isalpha():
-        print('O nome deve conter apenas letras.')
-    else:
-        break
--- exerc-cios/fiap - python/tempCodeRunnerFile.py
+++ exerc-cios/fiap - python/tempCodeRunnerFile.py
@@ -1,3 +1,5 @@
+import random
+
 lista = []
 
 while len(lista) < 10:
