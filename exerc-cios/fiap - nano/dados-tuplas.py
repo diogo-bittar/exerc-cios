873f80176ce48977a8939079e45e67151c8d4af8
Diogo Bittar Pedro <bittard58@gmail.com>
2026-05-14 11:25:51 -0300

    nano course

---
+++ exerc-cios/fiap - nano/dados-tuplas.py
@@ -0,0 +1,8 @@
+import funcoes
+
+users = []
+
+perguntar()
+
+while opcao == "I" or opcao == "P" or opcao == "E" or opcao == "L":
+    ...
---
+++ exerc-cios/fiap - nano/funcoes.py
@@ -0,0 +1,6 @@
+def perguntar():
+    return input("O que você deseja fazer?\n"+
+              "(I) --> Inserir um usuário.\n"+
+              "(P) --> Pesquisar um usuário\n"+
+              "(E) --> Excluir um usuário\n"+
+              "(L) --> Listar um usuário: ").upper()
